328f00ae01cd9a9721bcd83f2b244dcad59ad20c
bind <douglasjbinder@gmail.com>
2021-01-14 22:30:26 -0600

    cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const port = 8080;
+
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
